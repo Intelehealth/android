472a7282fdef4b1d82723b6437f99628e3b6078a
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-11-07 23:40:32 +0530

    Removed unused imports

--- videolibrary/src/main/java/org/intelehealth/videolibrary/listing/fragment/VideoFragment.kt
+++ videolibrary/src/main/java/org/intelehealth/videolibrary/listing/fragment/VideoFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.intelehealth.videolibrary.R
 import org.intelehealth.videolibrary.callbacks.VideoClickedListener
