fdcabf4092d3e09ef197f5443a4b43ae0a764ac3
vaibhav singh <vaibhav.singh@synechron.com>
2024-12-16 10:34:48 +0530

    Code update

--- app/src/main/java/org/intelehealth/app/ui/rosterquestionnaire/model/RoasterViewQuestion.kt
+++ app/src/main/java/org/intelehealth/app/ui/rosterquestionnaire/model/RoasterViewQuestion.kt
@@ -1,7 +1,9 @@
 package org.intelehealth.app.ui.rosterquestionnaire.model
 
+import org.intelehealth.app.ui.rosterquestionnaire.utilities.RoasterQuestionView
+
 data class RoasterViewQuestion(
-    val layoutId: Int,
+    val layoutId: RoasterQuestionView,
     val data: Any? = null,
     val question: String,
     val spinnerItem: List<String>?= null,
---
+++ app/src/main/java/org/intelehealth/app/ui/rosterquestionnaire/utilities/RoasterQuestionView.kt
@@ -0,0 +1,8 @@
+package org.intelehealth.app.ui.rosterquestionnaire.utilities
+
+import org.intelehealth.app.R
+
+enum class RoasterQuestionView(val lavout : Int) {
+    SPINNER(R.layout.item_spinner_view),
+    DATE_PICKER(R.layout.item_date_picker_view)
+}
